65f3533837157043546d23b692aaa81f8942c29d
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2021-03-19 15:24:55 +0600

    Update SacnnerClass.java

--- SacnnerClass.java
+++ SacnnerClass.java
@@ -3,24 +3,21 @@ public class SacnnerClass {
 	/*
 	 * import java.util.Scanner;
 	 * Method	Description
-     * nextBoolean()	Reads a boolean value from the user
-	 * nextByte()	Reads a byte value from the user
-	 * nextDouble()	Reads a double value from the user
-	 * nextFloat()	Reads a float value from the user
-     * nextInt()	Reads a int value from the user
-     * nextLine()	Reads a String value from the user
-     * nextLong()	Reads a long value from the user
-     * nextShort()	Reads a short value from the user
+         * nextBoolean()	Reads a boolean value from the user
+	 * nextByte()	        Reads a byte value from the user
+	 * nextDouble()		Reads a double value from the user
+	 * nextFloat()		Reads a float value from the user
+         * nextInt()		Reads a int value from the user
+         * nextLine()		Reads a String value from the user
+         * nextLong()		Reads a long value from the user
+         * nextShort()		Reads a short value from the user
 	 * 
-	 * //Input until EOF(End of File)//   
-     * while(sc.hasNext()){
-     *     //rest of the code for every input
-     * }
-     * //Input until EOF for integer//
-     * while(sc.hasNextInt())
-     * {
-     *     ... ...
-     * }
+	 * /
+	 /*
+	 https://ideone.com/5yyd6P
+	 https://ideone.com/f5bxjo
+	 https://ideone.com/5e7iMI
+	 
 	 */
 
 }
