527cbebf4f36e02f85c50dfedbc2eed4540b0dc7
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2022-01-30 18:30:21 +0600

    Update InputStreamBufferedReader.java

--- InputStreamBufferedReader.java
+++ InputStreamBufferedReader.java
@@ -14,6 +14,10 @@ public class InputStreamBufferedReader {
 		catch(Exception e) {
 			
 		}
+		finally{
+			r.close();
+        		br.close();
+		}
 		
 	}
 	*/
@@ -26,6 +30,8 @@ public class InputStreamBufferedReader {
 		String s;
 		s = br.readLine();
 		System.out.println(s);
+		r.close();
+        	br.close();
 	}
 	*/
 	// EOF
@@ -42,6 +48,10 @@ public class InputStreamBufferedReader {
 		}
 		catch(Exception e) {
  
+		}
+		finally{
+			r.close();
+        		br.close();
 		}
  
 	}
